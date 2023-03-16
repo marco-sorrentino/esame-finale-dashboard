fde5c767da4ec2e4068eec3dce55a60b814d05f0
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-16 15:22:34 +0100

    .

--- src/components/CalendarHomepage/CalendarHome.jsx
+++ src/components/CalendarHomepage/CalendarHome.jsx
@@ -3,14 +3,20 @@ import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-
 import listPlugin from "@fullcalendar/list";
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export const CalendarHome = () => {
   const [currentEvent, setCurrentEvent] = useState([]);
+  const [show, setShow] = useState(false);
+  console.log(currentEvent);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleDateClick = (selected) => {
+    handleShow();
     const title = prompt("please enter date");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
@@ -37,6 +43,20 @@ export const CalendarHome = () => {
 
   return (
     <div className="bgAllDiv mt-3 p-3">
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="calendar">
         <FullCalendar
           plugins={[
