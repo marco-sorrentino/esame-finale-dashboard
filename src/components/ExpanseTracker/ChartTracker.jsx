97dbbd89fc5a98cbfa223285ae85ce7e03c496cf
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-23 11:58:23 +0100

    fixed budget chart

---
+++ src/components/ExpanseTracker/ChartTracker.jsx
@@ -0,0 +1,71 @@
+import { useSelector } from "react-redux";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Line,
+  LineChart,
+} from "recharts";
+
+export const ChartTracker = () => {
+  const seeMoney = useSelector((state) => state.dashboard.planner);
+  console.log("TOTALE", seeMoney);
+  const moin = [];
+  const plus = [];
+  seeMoney.map((el) => (el.cost > 0 ? plus.push(el.cost) : moin.push(el.cost)));
+  const sumPlus = plus.reduce((accum, obj) => accum + obj, 0);
+  const sumMoin = moin.reduce((accum, obj) => accum + obj, 0);
+
+  const data = [
+    seeMoney?.map((el, i) => {
+      return { totale: el.cost, key: i };
+    }),
+  ];
+
+  return (
+    <>
+      {data && (
+        <ResponsiveContainer
+          className={"bgAllDiv  mt-3"}
+          width="100%"
+          height="100%"
+        >
+          <AreaChart
+            className=""
+            width={500}
+            height={400}
+            data={seeMoney?.map((el, i) => {
+              return {
+                name: el.text,
+                cost: el.cost,
+                key: i,
+              };
+            })}
+            style={{ position: "relative", height: "195px" }}
+          >
+            <CartesianGrid strokeDasharray="12 12" strokeOpacity={0.1} />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <defs>
+              <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#1CFEBA" stopOpacity="1" />
+                <stop offset="100%" stopColor="#62ff11" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+            <Area
+              type="monotone"
+              stroke="none"
+              dataKey="cost"
+              fill="url(#colorGradient)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      )}
+    </>
+  );
+};
--- src/components/ExpanseTracker/ExpanseTrackerHome.jsx
+++ src/components/ExpanseTracker/ExpanseTrackerHome.jsx
@@ -3,6 +3,7 @@ import { Col, Row } from "react-bootstrap";
 import { Budget } from "./Budget";
 import { ChartCake } from "./ChartCake";
 import { Calculator } from "./Calculator";
+import { ChartTracker } from "./ChartTracker";
 
 export const ExpanseTrackerHome = () => {
   return (
@@ -16,9 +17,11 @@ export const ExpanseTrackerHome = () => {
         <Col xs={12} md={4}>
           <ChartCake />
         </Col>
-        <Col xs={12} md={4}>
-          <Calculator />
+        <Col xs={12} md={8}>
+          <ChartTracker />
         </Col>
+      </Row>
+      <Row>
         <Col></Col>
       </Row>
     </div>
