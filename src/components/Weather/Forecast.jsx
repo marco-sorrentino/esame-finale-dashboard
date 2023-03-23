8248d4abff5fa42f606be4f6c2d7236b3a9007c9
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-23 10:59:49 +0100

    Forecast fixed

--- src/components/Weather/Forecast.jsx
+++ src/components/Weather/Forecast.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 export const Forecast = () => {
   const hour = useSelector((state) => state.dashboard.moreWeather);
+  console.log(hour);
 
   return (
     <>
@@ -10,26 +11,28 @@ export const Forecast = () => {
         <p className="titleTasks">Week's Highlights</p>
       </div>
       <div className="mt-3 d-flex">
-        <Row className="">
+        <Row className="mobileCities">
           {hour.list
-            .filter((_, i) => i % 8 === 0)
+            .filter((_, i) => i % 4 === 0)
             .map((el) => {
               return (
                 <Col>
                   <div className="bgAllDiv p-3  d-flex flex-column align-items-center justify-content-center">
                     <p className="fw-bold">{el.dt_txt.slice(6, -8)}</p>
+                    <p className="hour">{el.dt_txt.slice(11, -3)}</p>
                     <img
-                      style={{ width: "4em" }}
+                      style={{ width: "6em" }}
                       src={`http://openweathermap.org/img/wn/${el.weather[0].icon}@2x.png`}
                       alt=""
                     />
                     <div className="d-flex justify-content-between">
-                      <p className="">{el.main.temp_max.toFixed()}</p>
+                      <p className="">{el.main.temp_max.toFixed()}°</p>
                     </div>
                   </div>
                 </Col>
               );
-            })}
+            })
+            .slice(0, -3)}
         </Row>
       </div>
     </>
--- src/components/Weather/General.jsx
+++ src/components/Weather/General.jsx
@@ -10,18 +10,19 @@ export const General = () => {
   const location = { timeZone: "Europe/Rome" };
   const hourItaly = now.toLocaleTimeString("it-IT", location);
   return (
-    <div className="bgAllDiv p-3 weatherCard">
+    <div className="bgAllDiv p-3 weatherCard d-flex flex-column justify-content-between">
       <p className="weatherTitle">General</p>
       <div className="d-flex align-items-center">
-        <p className="conditionWeather">
+        <p className="conditionWeather mobileWeatherText">
           {weatherCity.weather[0].description.charAt(0).toUpperCase() +
             weatherCity.weather[0].description.slice(1)}
         </p>
         <img
           src={`http://openweathermap.org/img/wn/${weatherCity.weather[0].icon}@2x.png`}
           alt=""
+          className="weatherImageMobile"
         />
-        <p className="conditionWeather">
+        <p className="conditionWeather mobileWeatherText">
           {weatherCity?.main?.temp?.toFixed()} ° C
         </p>
       </div>
