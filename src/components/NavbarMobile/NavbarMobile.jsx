016e6fa1b9aca6731e4aa9f3a8d0420cbdff0b1f
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-03-29 10:21:06 +0200

    fixed navbar

--- src/components/NavbarMobile/NavbarMobile.jsx
+++ src/components/NavbarMobile/NavbarMobile.jsx
@@ -3,17 +3,12 @@ import { Container, NavDropdown } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../assets/images/LOGO.svg";
+import { Link } from "react-router-dom";
 
 export const NavbarMobile = () => {
   return (
     <Container fluid>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light"
-        className="d-lg-none"
-      >
+      <Navbar collapseOnSelect expand="lg" className="d-lg-none bgAllDiv mt-2">
         <Container>
           <Navbar.Brand className="logoText" href="#home">
             <img src={logo} alt="" className="me-2" />
@@ -22,13 +17,13 @@ export const NavbarMobile = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Home</Nav.Link>
-              <Nav.Link href="#pricing">Calendar</Nav.Link>
-              <Nav.Link href="#pricing">Tasks</Nav.Link>
-              <Nav.Link href="#pricing">Expanse Tracker</Nav.Link>
-              <Nav.Link href="#pricing">Finance</Nav.Link>
-              <Nav.Link href="#pricing">News</Nav.Link>
-              <Nav.Link href="#pricing">Weather</Nav.Link>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/calendar"}>Calendar</Link>
+              <Link to={"/tasks"}>Tasks</Link>
+              <Link to={"/expanse-tracker"}>Expense Tracker</Link>
+              <Link to={"/finance"}>Finance</Link>
+              <Link to={"/heading-news"}>News</Link>
+              <Link to={"/weather"}>Weather</Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
